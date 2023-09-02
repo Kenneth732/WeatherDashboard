935812a9783449e5a93d24ed8fabc7290b3c5dc9
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-09-02 06:52:19 +0300

    js selectors city, search, weatherIcon, descr, temp, hummid, wind, forecast

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {})
+document.addEventListener('DOMContentLoaded', () => {
+    const cityInput = document.getElementById('cityInput');
+    const searchButton = document.getElementById('searchButton');
+    const weatherIcon = document.getElementById('weatherIcon');
+    const description = document.getElementById('description');
+    const temperature = document.getElementById('temperature');
+    const humidity = document.getElementById('humidity');
+    const windSpeed = document.getElementById('windSpeed');
+    const forecastList = document.getElementById('forecastList');
+})
