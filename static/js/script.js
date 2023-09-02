3f116b35b1d6c5a313eb2eb4db9f2e6f9e44af58
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-09-02 06:54:33 +0300

    js getWeatherData(city)

--- static/js/script.js
+++ static/js/script.js
@@ -16,6 +16,22 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     async function getWeatherData(city){
-        
+        try {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`);
+            const data = await response.json();
+
+            const { main, weather, wind } = data;
+            const [weatherData] = weather;
+
+            weatherIcon.style.backgroundImage = `url('https://openweathermap.org/img/w/${weatherData.icon}.png')`;
+            description.textContent = weatherData.description;
+            temperature.textContent = main.temp;
+            humidity.textContent = main.humidity;
+            windSpeed.textContent = wind.speed;
+
+            getForecastData(city);
+        } catch (error) {
+            console.error('Error fetching weather data:', error);
+        }
     }
 })
