c8ec617cd6fbb87d8a51f71caa48d2221a5a4e5a
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-09-02 06:55:33 +0300

    js getWeatherData(city)

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const apiKey = 'YOUR_API_KEY'; // Replace with your OpenWeatherMap API key
+
     const cityInput = document.getElementById('cityInput');
     const searchButton = document.getElementById('searchButton');
     const weatherIcon = document.getElementById('weatherIcon');
@@ -15,7 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    async function getWeatherData(city){
+    async function getWeatherData(city) {
         try {
             const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`);
             const data = await response.json();
@@ -34,4 +36,33 @@ document.addEventListener('DOMContentLoaded', () => {
             console.error('Error fetching weather data:', error);
         }
     }
-})
+
+    async function getForecastData(city) {
+        try {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`);
+            const data = await response.json();
+
+            const { list } = data;
+
+            forecastList.innerHTML = '';
+
+            for (const forecast of list.slice(0, 5)) {
+                const { dt_txt, main, weather } = forecast;
+                const [weatherData] = weather;
+
+                const forecastItem = document.createElement('div');
+                forecastItem.classList.add('forecastItem');
+                forecastItem.innerHTML = `
+                    <p>Date: ${dt_txt.split(' ')[0]}</p>
+                    <p>Time: ${dt_txt.split(' ')[1]}</p>
+                    <p>Description: ${weatherData.description}</p>
+                    <p>Temperature: ${main.temp} &deg;C</p>
+                `;
+
+                forecastList.appendChild(forecastItem);
+            }
+        } catch (error) {
+            console.error('Error fetching forecast data:', error);
+        }
+    }
+});
