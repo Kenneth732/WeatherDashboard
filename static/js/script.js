41004a275c38c572e0cb15db1f97ae34c64b1809
KENNETH <kenneth.mburu@student.moringaschool.com>
2023-09-18 15:41:42 +0300

    Update script.js
    change the conflix var in display func

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const apiKey = 'b374050776d5cda6d0c789791abb3cd2';
 
-
     const cityInput = document.getElementById('cityInput');
     const searchButton = document.getElementById('searchButton');
     const weatherIcon = document.getElementById('weatherIcon');
@@ -48,8 +47,8 @@ document.addEventListener('DOMContentLoaded', () => {
             forecastList.innerHTML = '';
 
             for (const forecast of list.slice(0, 5)) {
-                const { dt_txt, main, weather } = forecast;
-                const [weatherData] = weather;
+                const { dt_txt, main: forecastMain, weather: forecastWeather } = forecast;
+                const [weatherData] = forecastWeather;
 
                 const forecastItem = document.createElement('div');
                 forecastItem.classList.add('forecastItem');
@@ -57,7 +56,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     <p>Date: ${dt_txt.split(' ')[0]}</p>
                     <p>Time: ${dt_txt.split(' ')[1]}</p>
                     <p>Description: ${weatherData.description}</p>
-                    <p>Temperature: ${main.temp} &deg;C</p>
+                    <p>Temperature: ${forecastMain.temp} &deg;C</p>
                 `;
 
                 forecastList.appendChild(forecastItem);
@@ -67,3 +66,95 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const apiKey = 'b374050776d5cda6d0c789791abb3cd2';
+
+
+//     const cityInput = document.getElementById('cityInput');
+//     const searchButton = document.getElementById('searchButton');
+//     const weatherIcon = document.getElementById('weatherIcon');
+//     const description = document.getElementById('description');
+//     const temperature = document.getElementById('temperature');
+//     const humidity = document.getElementById('humidity');
+//     const windSpeed = document.getElementById('windSpeed');
+//     const forecastList = document.getElementById('forecastList');
+
+//     searchButton.addEventListener('click', () => {
+//         const city = cityInput.value;
+//         if (city) {
+//             getWeatherData(city);
+//         }
+//     });
+
+//     async function getWeatherData(city) {
+//         try {
+//             const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`);
+//             const data = await response.json();
+
+//             const { main, weather, wind } = data;
+//             const [weatherData] = weather;
+
+//             weatherIcon.style.backgroundImage = `url('https://openweathermap.org/img/w/${weatherData.icon}.png')`;
+//             description.textContent = weatherData.description;
+//             temperature.textContent = main.temp;
+//             humidity.textContent = main.humidity;
+//             windSpeed.textContent = wind.speed;
+
+//             getForecastData(city);
+//         } catch (error) {
+//             console.error('Error fetching weather data:', error);
+//         }
+//     }
+
+//     async function getForecastData(city) {
+//         try {
+//             const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`);
+//             const data = await response.json();
+
+//             const { list } = data;
+
+//             forecastList.innerHTML = '';
+
+//             for (const forecast of list.slice(0, 5)) {
+//                 const { dt_txt, main, weather } = forecast;
+//                 const [weatherData] = weather;
+
+//                 const forecastItem = document.createElement('div');
+//                 forecastItem.classList.add('forecastItem');
+//                 forecastItem.innerHTML = `
+//                     <p>Date: ${dt_txt.split(' ')[0]}</p>
+//                     <p>Time: ${dt_txt.split(' ')[1]}</p>
+//                     <p>Description: ${weatherData.description}</p>
+//                     <p>Temperature: ${main.temp} &deg;C</p>
+//                 `;
+
+//                 forecastList.appendChild(forecastItem);
+//             }
+//         } catch (error) {
+//             console.error('Error fetching forecast data:', error);
+//         }
+//     }
+// });
